e4f33cb86ebbdbdd1d037bac611a621c18f41738
Glen Sturge <117656215+glen-sturge@users.noreply.github.com>
2023-12-14 10:28:05 -0330

    added eslint + plugins

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,87 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import RowHouse from "../assets/row_house.png";
 
 const Header = () => {
-  const [activeButton, setActiveButton] = useState('HOME');
+  const [activeButton, setActiveButton] = useState("HOME");
 
   const handleButtonClick = (buttonName) => {
     setActiveButton(buttonName === activeButton ? null : buttonName);
   };
 
   return (
-    <div className="bg-avion-blue flex h-40  max-w-full justify-center">
+    <div className="flex h-40 max-w-full  justify-center bg-avion-blue">
       <div className="flex w-full max-w-7xl ">
         <div className="flex w-1/2 flex-row items-center">
           <img className="ml-6 h-28" src={RowHouse} alt="row houses" />
 
           <div className=" ml-2 flex flex-col justify-center text-white">
-            <h1 className="font-Koulen text-4xl">ST. JOHN'S</h1>
-            <h2 className="font-Gupter text-1xl text-opacity-70">
+            <h1 className="font-Koulen text-4xl">ST. JOHN&apos;S</h1>
+            <h2 className="text-1xl font-Gupter text-opacity-70">
               International Airport Authority
             </h2>
           </div>
         </div>
         <div className="w-1/2">
           <ul className="flex h-full w-full flex-row items-center justify-around text-3xl text-white text-opacity-70">
-          <button onClick={() => handleButtonClick('HOME')} style={{color: activeButton === 'HOME' ? 'white' :'rgba(255, 255, 255, 0.63)'}}>HOME</button>
+            <button
+              onClick={() => handleButtonClick("HOME")}
+              style={{
+                color:
+                  activeButton === "HOME"
+                    ? "white"
+                    : "rgba(255, 255, 255, 0.63)",
+              }}
+            >
+              HOME
+            </button>
             <li>|</li>
-            <button onClick={() => handleButtonClick('AMENITIES')} style={{color: activeButton === 'AMENITIES' ? 'white' :'rgba(255, 255, 255, 0.63)'}}>AMENITIES</button>
+            <button
+              onClick={() => handleButtonClick("AMENITIES")}
+              style={{
+                color:
+                  activeButton === "AMENITIES"
+                    ? "white"
+                    : "rgba(255, 255, 255, 0.63)",
+              }}
+            >
+              AMENITIES
+            </button>
             <li>|</li>
-            <button onClick={() => handleButtonClick('FLIGHTS')} style={{color: activeButton === 'FLIGHTS' ? 'white' :'rgba(255, 255, 255, 0.63)'}}>FLIGHTS</button>
+            <button
+              onClick={() => handleButtonClick("FLIGHTS")}
+              style={{
+                color:
+                  activeButton === "FLIGHTS"
+                    ? "white"
+                    : "rgba(255, 255, 255, 0.63)",
+              }}
+            >
+              FLIGHTS
+            </button>
             <li>|</li>
-            <button onClick={() => handleButtonClick('ADMIN')} style={{color: activeButton === 'ADMIN' ? 'white' :'rgba(255, 255, 255, 0.63)'}}>ADMIN</button>
+            <button
+              onClick={() => handleButtonClick("ADMIN")}
+              style={{
+                color:
+                  activeButton === "ADMIN"
+                    ? "white"
+                    : "rgba(255, 255, 255, 0.63)",
+              }}
+            >
+              ADMIN
+            </button>
             <li>|</li>
-            <button onClick={() => handleButtonClick('CONTACT')} style={{color: activeButton === 'CONTACT' ? 'white' :'rgba(255, 255, 255, 0.63)'}}>CONTACT</button>
+            <button
+              onClick={() => handleButtonClick("CONTACT")}
+              style={{
+                color:
+                  activeButton === "CONTACT"
+                    ? "white"
+                    : "rgba(255, 255, 255, 0.63)",
+              }}
+            >
+              CONTACT
+            </button>
           </ul>
         </div>
       </div>
--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ChristmasPhoto from "../assets/a_background_of_a_christmas_scene_with_a_christm.jpg";
 import NewfoundlandPhoto from "../assets/erik-mclean-oc7I7D3bjw0-unsplash.jpg";
 
@@ -20,22 +20,48 @@ const Splash = () => {
     <div className="flex w-full flex-row items-center justify-center">
       {showDiv1 && (
         <div>
-        <div><img className="mb-4 mt-4 h-96" src={NewfoundlandPhoto} alt="christmas photo"></img></div>
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-LovedbytheKing text-white text-5xl">
-          Explore Newfoundland
+          <div>
+            <img
+              className="mb-4 mt-4 h-96"
+              src={NewfoundlandPhoto}
+              alt="christmas photo"
+            ></img>
+          </div>
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform font-LovedbytheKing text-5xl text-white">
+            Explore Newfoundland
           </div>
         </div>
       )}
       {showDiv2 && (
         <div>
-        <div><img className="mb-4 mt-4 h-96" src={ChristmasPhoto} alt="christmas photo"></img></div>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-LovedbytheKing text-white text-5xl">
-          Happy Holidays!
+          <div>
+            <img
+              className="mb-4 mt-4 h-96"
+              src={ChristmasPhoto}
+              alt="christmas photo"
+            ></img>
+          </div>
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform font-LovedbytheKing text-5xl text-white">
+            Happy Holidays!
+          </div>
         </div>
-      </div>
       )}
-      <button><div onClick={handleToggleDiv2} className="absolute top-1/2.5 left-1/3 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl">&lt;</div></button>
-      <button><div onClick={handleToggleDiv1} className="absolute top-1/2.5 right-1/3 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl">&gt;</div></button>
+      <button>
+        <div
+          onClick={handleToggleDiv2}
+          className="top-1/2.5 absolute left-1/3 -translate-x-1/2 -translate-y-1/2 transform text-3xl text-white"
+        >
+          &lt;
+        </div>
+      </button>
+      <button>
+        <div
+          onClick={handleToggleDiv1}
+          className="top-1/2.5 absolute right-1/3 -translate-x-1/2 -translate-y-1/2 transform text-3xl text-white"
+        >
+          &gt;
+        </div>
+      </button>
     </div>
   );
 };
