36737a8968185922fb28a6d1a2c0b3ea7f966852
Glen Sturge <117656215+glen-sturge@users.noreply.github.com>
2023-12-15 11:59:42 -0330

    added prop-types and arrivalTable design

--- src/App.js
+++ src/App.js
@@ -5,6 +5,100 @@ import Header from "./components/Header";
 import Splash from "./components/Splash";
 import FlightViewButtons from "./components/FlightViewButtons";
 // import Admin from "./components/Admin";
+import ArrivalTable from "./components/ArrivalTable";
+
+const flights = [
+  {
+    arrivalDate: "Dec 21",
+    departureDate: "",
+    arrivalTime: "00:15",
+    departureTime: "",
+    flightNumber: "AL0001",
+    airline: "American Airlines",
+    to: null,
+    from: "Chicago",
+    status: "ARRIVED",
+    terminal: "West",
+    gate: "B2",
+    checkInCounter: null,
+    carousel: "B",
+  },
+  {
+    arrivalDate: "Dec 21",
+    departureDate: "",
+    arrivalTime: "00:15",
+    departureTime: "",
+    flightNumber: "AL0001",
+    airline: "American Airlines",
+    to: null,
+    from: "Chicago",
+    status: "ARRIVED",
+    terminal: "West",
+    gate: "B2",
+    checkInCounter: null,
+    carousel: "B",
+  },
+  {
+    arrivalDate: "Dec 21",
+    departureDate: "",
+    arrivalTime: "00:15",
+    departureTime: "",
+    flightNumber: "AL0001",
+    airline: "American Airlines",
+    to: null,
+    from: "Chicago",
+    status: "ARRIVED",
+    terminal: "West",
+    gate: "B2",
+    checkInCounter: null,
+    carousel: "B",
+  },
+  {
+    arrivalDate: "Dec 21",
+    departureDate: "",
+    arrivalTime: "00:15",
+    departureTime: "",
+    flightNumber: "AL0001",
+    airline: "American Airlines",
+    to: null,
+    from: "Chicago",
+    status: "ARRIVED",
+    terminal: "West",
+    gate: "B2",
+    checkInCounter: null,
+    carousel: "B",
+  },
+  {
+    arrivalDate: "Dec 21",
+    departureDate: "",
+    arrivalTime: "00:15",
+    departureTime: "",
+    flightNumber: "AL0001",
+    airline: "American Airlines",
+    to: null,
+    from: "Chicago",
+    status: "ARRIVED",
+    terminal: "West",
+    gate: "B2",
+    checkInCounter: null,
+    carousel: "B",
+  },
+  {
+    arrivalDate: "Dec 21",
+    departureDate: "",
+    arrivalTime: "00:15",
+    departureTime: "",
+    flightNumber: "AL0001",
+    airline: "American Airlines",
+    to: null,
+    from: "Chicago",
+    status: "ARRIVED",
+    terminal: "West",
+    gate: "B2",
+    checkInCounter: null,
+    carousel: "B",
+  },
+];
 
 function App() {
   return (
@@ -14,7 +108,11 @@ function App() {
       {/* <Admin /> */}
       {/* <Login /> */}
       <Banner />
+      <div className="flex w-full justify-center">
+        <ArrivalTable flights={flights} />
+      </div>
       <FlightViewButtons />
+
       <Footer />
     </div>
   );
---
+++ src/components/ArrivalTable.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ArrivalTable = ({ flights }) => {
+  return (
+    <table className="w-full max-w-screen-lg table-fixed border-2 border-red-500 text-center">
+      <thead className="border-y-14 border-avion-blue bg-avion-blue font-Gupter text-2xl text-white text-opacity-70">
+        <tr>
+          <th className="border-r border-white">Airline</th>
+          <th className="border-r border-white">Flight Number</th>
+          <th className="border-r border-white">Date</th>
+          <th className="border-r border-white">Time</th>
+          <th className="border-r border-white">From</th>
+          <th className="">Status</th>
+        </tr>
+      </thead>
+      <tbody className="">
+        {flights.length > 0 ? (
+          flights.map((flight, index) => {
+            console.log(flight);
+            return (
+              <tr
+                key={index}
+                className={`${
+                  index % 2 === 0 ? "bg-gray-200" : "bg-white"
+                } font-Gupter`}
+              >
+                <td>{flight.airline}</td>
+                <td>{flight.flightNumber}</td>
+                <td>{flight.arrivalDate}</td>
+                <td>{flight.arrivalTime}</td>
+                <td>{flight.from}</td>
+                <td>{flight.status}</td>
+              </tr>
+            );
+          })
+        ) : (
+          <tr>
+            <td colSpan="5">Loading...</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
+};
+
+ArrivalTable.propTypes = {
+  flights: PropTypes.array.isRequired,
+};
+
+export default ArrivalTable;
