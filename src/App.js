0d169bbf6c42350cb2d7652a8e90a31a00397294
Laura Fry <laurafry@Lauras-MacBook-Pro.local>
2023-12-15 12:05:04 -0330

    app.js file fix

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Splash from "./components/Splash";
-import FlightViewButtons from "./components/FlightViewButtons";
-// import Admin from "./components/Admin";
+// import FlightViewButtons from "./components/FlightViewButtons";
+import Admin from "./components/Admin";
+import Contact from "./components/Contact";
+
+function Home() {
+  return (
+    <>
+      <Splash />
+      <Banner />
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="h-screen font-Koulen tracking-wider">
-      <Header />
-      <Splash />
-      {/* <Admin /> */}
-      <Banner />
-      <FlightViewButtons />
-      <Footer />
-    </div>
+    <Router>
+      <div className="h-screen font-Koulen tracking-wider">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/flights" element={<Banner />}></Route>
+          <Route path="/admin" element={<Admin />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import FlightIcon from "../assets/FLIGHT HUB (1).png"
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [activeButton, setActiveButton] = useState("HOME");
@@ -24,6 +25,7 @@ const Header = () => {
         </div>
         <div className="w-1/2">
           <ul className="flex h-full w-full flex-row items-center justify-around text-3xl text-white text-opacity-70">
+            <Link to="/">
             <button
               onClick={() => handleButtonClick("HOME")}
               style={{
@@ -35,7 +37,9 @@ const Header = () => {
             >
               HOME
             </button>
+            </Link>
             <li>|</li>
+            <Link to="/flights">
             <button
               onClick={() => handleButtonClick("FLIGHTS")}
               style={{
@@ -47,7 +51,9 @@ const Header = () => {
             >
               FLIGHTS
             </button>
+            </Link>
             <li>|</li>
+            <Link to="/admin">
             <button
               onClick={() => handleButtonClick("ADMIN")}
               style={{
@@ -59,7 +65,9 @@ const Header = () => {
             >
               ADMIN
             </button>
+            </Link>
             <li>|</li>
+            <Link to="/contact">
             <button
               onClick={() => handleButtonClick("CONTACT")}
               style={{
@@ -71,6 +79,7 @@ const Header = () => {
             >
               CONTACT
             </button>
+            </Link>
           </ul>
         </div>
       </div>
