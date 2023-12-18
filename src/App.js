c0fc9a81ccd4596a4b7602e2243aed6335fdc86c
Glen Sturge <117656215+glen-sturge@users.noreply.github.com>
2023-12-18 02:26:49 -0330

    updated fetch for flightdata and ArrivalTable to use it.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Splash from "./components/Splash";
 import Admin from "./components/Admin";
+import ArrivalTable from "./components/ArrivalTable";
 
 function App() {
+  const [flightData, setFlightData] = useState([]);
+
+  useEffect(() => {
+    const fetchFlightData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8080/arrival_flight_view?arrivalAirportName=St.%20John%27s%20International%20Airport`,
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch flight data");
+        }
+        const data = await response.json();
+        console.log(data);
+        setFlightData(data);
+      } catch (error) {
+        console.error("Error fetching flight data:", error.message);
+      }
+    };
+
+    fetchFlightData();
+  }, []);
+
+  // const [flights, setFlights] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchFlights = async () => {
+  //     try {
+  //       const response = await fetch("http://localhost:8080/flight");
+  //       if (!response.ok) {
+  //         throw new Error("Failed to fetch flights");
+  //       }
+
+  //       const data = await response.json();
+  //       const flightsData = data._embedded.flight || [];
+  //       setFlights(flightsData);
+  //     } catch (error) {
+  //       console.error("Error fetching flights:", error.message);
+  //     }
+  //   };
+
+  //   fetchFlights();
+  // }, []);
+
   return (
     <div className="h-screen font-Koulen tracking-wider">
       <Header />
       <Splash />
-      {/* <Admin /> */}
+      <Admin />
       <Banner />
       <Footer />
+      <div className="flex w-full justify-center">
+        <ArrivalTable flightData={flightData} />
+      </div>
     </div>
   );
 }
---
+++ src/components/ArrivalTable.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ArrivalTable = ({ flightData }) => {
+  return (
+    <table className="w-full max-w-screen-lg table-fixed border-2 border-red-500 text-center">
+      <thead className="border-y-14 border-avion-blue bg-avion-blue font-Gupter text-2xl text-white text-opacity-70">
+        <tr>
+          <th className="border-r border-white">Airline</th>
+          <th className="border-r border-white">Flight Number</th>
+          <th className="border-r border-white">Date</th>
+          <th className="border-r border-white">Time</th>
+          <th className="border-r border-white">From</th>
+          <th className="">Status</th>
+        </tr>
+      </thead>
+      <tbody className="">
+        {flightData.length > 0 ? (
+          flightData.map((flight, index) => {
+            console.log(flight);
+
+            const arrivalDateObject = new Date(flight.arrivalDateAndTime);
+            // console.log(arrivalDateObject);
+            const arrivalDate = arrivalDateObject.toLocaleDateString();
+            // console.log(arrivalDate);
+            const arrivalTime = arrivalDateObject.toLocaleTimeString();
+            // console.log(arrivalTime);
+
+            return (
+              <tr
+                key={index}
+                className={`${
+                  index % 2 === 0 ? "bg-gray-200" : "bg-white"
+                } font-Gupter`}
+              >
+                <td>{flight.airlineName}</td>
+                <td>{flight.flightNumber}</td>
+                <td>{arrivalDate}</td>
+                <td>{arrivalTime}</td>
+                <td>{flight.departureAirportName}</td>
+                <td>{flight.status}</td>
+              </tr>
+            );
+          })
+        ) : (
+          <tr>
+            <td colSpan="5">Loading...</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
+};
+
+ArrivalTable.propTypes = {
+  flightData: PropTypes.array.isRequired,
+};
+
+export default ArrivalTable;
